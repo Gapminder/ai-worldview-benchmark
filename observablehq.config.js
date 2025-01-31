0017891eb521c7efe391eaa086923d67a3ea8e8d
angie <konzeptmeister@gmail.com>
2025-01-31 20:02:15 +0100

    Favicon and config

--- observablehq.config.js
+++ observablehq.config.js
@@ -17,7 +17,7 @@ export default {
   // ],
 
   // Content to add to the head of the page, e.g. for a favicon:
-  head: '<link rel="icon" href="observable.png" type="image/png" sizes="32x32">',
+  head: '<link rel="icon" href="favicon.png" type="image/png" sizes="32x32">',
 
   // The path to the source root.
   root: "src",
@@ -25,10 +25,10 @@ export default {
   // Some additional configuration options and their defaults:
   // theme: "default", // try "light", "dark", "slate", etc.
   // header: "", // what to show in the header (HTML)
-  // footer: "Built with Observable.", // what to show in the footer (HTML)
+  footer: `<div></div>`, // what to show in the footer (HTML)
   // sidebar: true, // whether to show the sidebar
   // toc: true, // whether to show the table of contents
-  // pager: true, // whether to show previous & next links in the footer
+  pager: false, // whether to show previous & next links in the footer
   // output: "dist", // path to the output root for build
   // search: true, // activate search
   // linkify: true, // convert URLs in Markdown to links
